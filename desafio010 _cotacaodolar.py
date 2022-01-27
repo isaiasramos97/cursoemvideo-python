bd25ad130dcdf110f3424ce8df2495cf752b39cd
Isaias <isaiasramos_@outlook.com>
2022-01-26 23:35:35 -0300

    cotacao atual
    novo desafio utilizando cotação atual

--- desafio010 _cotacaodolar.py
+++ desafio010 _cotacaodolar.py
@@ -1,14 +1,14 @@
 # ler quanto de dinheiro a pessoa tem e quanto ela pode comprar em dolar (5,20)
 
+import requests
+import json
 
+cotacoes = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
+cotacoes = cotacoes.json()
+cotacao_dolar = float(cotacoes['USDBRL']['bid'])
 
-
-
-
-
+print(f'O dólar neste momento vale R${cotacao_dolar:.2f}')
 print('Quer saber quanto você pode comprar em dólares?')
 reais = float(input('Quanto você tem em reais? R$'))
-usd = 5.20
 compra = reais / usd
-
 print(f'\nVocê pode comprar US${compra:.2f}.')
