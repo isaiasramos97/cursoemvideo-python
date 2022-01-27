e1c99454299288e1a955bf362970e65d12a74877
Isaias <isaiasramos_@outlook.com>
2022-01-27 00:10:38 -0300

    exclusao de linhas
    linhas desnecessarias

--- desafio019.py
+++ desafio019.py
@@ -4,20 +4,10 @@
 # usar random
 # o uso de str não é obrigatório
 
-import random
+from random import choice
 a1 = str(input('Primeiro aluno: '))
 a2 = str(input('Segundo aluno: '))
 a3 = str(input('Terceiro aluno: '))
 a4 = str(input('Quarto aluno: '))
 lista = [a1, a2, a3, a4]
-print(f'\nO aluno escolhido é {random.choice(lista)}')
-
-'''from random import choice
-a1 = str(input('Primeiro aluno: '))
-a2 = str(input('Segundo aluno: '))
-a3 = str(input('Terceiro aluno: '))
-a4 = str(input('Quarto aluno: '))
-a5 = str(input('Quinto aluno: '))
-a6 = str(input('Sexto aluno: '))
-lista = [a1, a2, a3, a4, a5, a6]
-print(f'\nO aluno escolhido é {choice(lista)}')'''
+print(f'\nO aluno escolhido é {choice(lista)}')
