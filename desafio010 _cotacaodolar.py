84e645f3744591940d890a4f45754d8aee95c38c
Isaias <isaiasramos_@outlook.com>
2022-01-26 23:51:04 -0300

    variaveis corrigidas

--- desafio010 _cotacaodolar.py
+++ desafio010 _cotacaodolar.py
@@ -9,6 +9,7 @@ cotacao_dolar = float(cotacoes['USDBRL']['bid'])
 
 print(f'O dólar neste momento vale R${cotacao_dolar:.2f}')
 print('Quer saber quanto você pode comprar em dólares?')
-reais = float(input('Quanto você tem em reais? R$'))
-compra = reais / usd
-print(f'\nVocê pode comprar US${compra:.2f}.')
+reais = str(input('Quanto você tem em reais? R$')).replace(',', '.')
+reais = float(reais)
+compra = reais / cotacao_dolar
+print(f'Você pode comprar US${compra:.2f}.')
