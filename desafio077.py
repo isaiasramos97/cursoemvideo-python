2e5072a686819f4c132f48468c507ec4c3be972d
Isaias <isaiasramos_@outlook.com>
2022-02-01 19:28:46 -0300

    adição de contagem de vogais com a biblioteca re, metodo findall

--- desafio077.py
+++ desafio077.py
@@ -1,10 +1,17 @@
 # crie um programa que tenha uma tupla com varias palavras (nao usar acentos).
 # depois disso, voce deve mostrar, para cada palavra, quais sao as suas vogais
-import string
-palavras = ('APRENDER','ESTUDAR', 'TRABALHAR', 'PYTHON', 'PARAISO',
+import re
+
+palavras = ('APRENDER', 'ESTUDAR', 'TRABALHAR', 'PYTHON', 'PARAISO',
             'CARTA', 'ENTREGA', 'VENDER', 'MERGULHO', 'OSSO')
 for p in palavras:
     print(f'\nNa palavra {p} temos ', end='')
     for letra in p:
         if letra in 'AEIOU':
             print(letra, end=' ')
+
+vogais = re.compile(r'[aeiouAEIOU]')  # utilizacao de regex. aprendido no livro "automatize tarefas maçantes com python"
+lista_das_vogais = vogais.findall(str(palavras))
+print()
+print('-='*30)
+print(f'{len(lista_das_vogais)} vogais')
